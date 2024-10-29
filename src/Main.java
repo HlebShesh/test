6457eaa4a4df6a9e136077be2fb907dc3bc4bd09
MuslleMummy <glebanator2000pro@gmail.com>
2024-10-29 19:44:01 +0300

    Initial project setup

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
-// TODO: we need addthe missing classes
+// TODO: we need add the missing classes
+// OK, i will add 'Adder' and 'Substractor' classes
 public class Main {
     public static void main(String[] args) {
         Adder adder = new Adder();
