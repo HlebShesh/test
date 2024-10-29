87162a5c437687dbec6e9411d0587b4277d8bf42
MuslleMummy <glebanator2000pro@gmail.com>
2024-10-29 19:19:31 +0300

    Initial project setup

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Adder adder = new Adder();
+        System.out.println(adder.add(1,2));
+        Substructor substructor = new Substructor();
+        System.out.println(substructor.substract(6,3));
+    }
+}
